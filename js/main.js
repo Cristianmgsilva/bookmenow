792843041ca07ee760baf5bab47f248562bb52c9
Cristian Manoel <cristiangcl2017@gmail.com>
2023-11-10 21:53:29 -0300

    personalizacao responssivo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const botaoMenu = document.getElementById('botao-menu-mobile');
+const menu = document.getElementById('menu');
+
+botaoMenu.addEventListener('click', function(){
+    menu.classList.toggle('menu-ativo')
+});
+
+
+
